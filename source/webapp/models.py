3bc944b705c563878084ea35e7676dcf18e347ab
tyzo4ka <tyzo4ka@gmail.com>
2019-12-14 11:51:23 +0600

    Added Comments model

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -7,8 +7,28 @@ class Photography(models.Model):
     caption = models.CharField(max_length=500, null=False, blank=False, verbose_name="Подпись")
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     rating = models.IntegerField(default=0, verbose_name='Рейтинг')
-    author = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False, related_name="photo",
-                               verbose_name="Автор")
+    photo_author = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False, related_name="photo",
+                               verbose_name="Автор фотографии")
+
+    def __str__(self):
+        return self.caption
+
+
+class Comments(models.Model):
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
+    photography = models.ForeignKey("webapp.Photography", related_name="comments", on_delete=models.CASCADE,
+                                    verbose_name="Фотография")
+    comments_author = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False, related_name="comments",
+                               verbose_name="Автор комментария")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.text
+
+
+
+
+
 
 
 
