0e4985914e4d0885c53ad58b48579cabadc5b94a
tyzo4ka <tyzo4ka@gmail.com>
2019-12-14 11:37:14 +0600

    Added Photography model

--- source/main/settings.py
+++ source/main/settings.py
@@ -122,3 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+MEDIA_URL = '/uploads/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Photography(models.Model):
+    photo = models.ImageField(null=False, blank=False, upload_to='photo', verbose_name="Фото")
+    caption = models.CharField(max_length=500, null=False, blank=False, verbose_name="Подпись")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    rating = models.IntegerField(default=0, verbose_name='Рейтинг')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False, related_name="photo",
+                               verbose_name="Автор")
+
+
+
+
+
