b457baee83a56c09634f6ee66bb6e3d3468ffba3
tyzo4ka <tyzo4ka@gmail.com>
2019-12-14 08:07:45 +0600

    Edited  settings

--- source/main/settings.py
+++ source/main/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "webapp",
+    "api",
+    "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
